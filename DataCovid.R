3725a44e8c03f42b5a83a66cffaa3156d43dfd31
Ulises.HdzG <p_ulises.18@hotmail.com>
2021-12-01 17:22:15 -0500

    Script that generates the COVID data. Additional files: COVID_vars_description that contains variable descriptions

---
+++ DataCovid.R
@@ -0,0 +1,27 @@
+# Contains COVID19 data sets from many countries
+library(COVID19)
+library(tidyverse)
+
+# Load data sets of Mexico and US
+mex<- covid19(country="MX")
+usa<- covid19(country="US")
+
+# percentage of NA
+mex_nas<-lapply(setNames(names(mex),names(mex)),function(x){
+  sum(!is.na(mex[,x]))
+})
+
+mex_nas<-data.frame(var=names(mex_nas),Non_missing=unlist(mex_nas),row.names = NULL)
+
+
+usa_nas<-lapply(setNames(names(usa),names(usa)),function(x){
+  sum(!is.na(usa[,x]))
+})
+
+usa_nas<-data.frame(var=names(usa_nas),Non_missing=unlist(usa_nas),row.names = NULL)
+
+compara<-usa_nas%>%
+  left_join(mex_nas,by = "var",suffix = c("_usa","_mx"))
+
+# Variables of interest:
+variables<-read.csv("~/Data/COVID_vars_description.csv")
